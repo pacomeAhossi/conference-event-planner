05f34e7250b883711e698861533ceaceb47d066a
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-03-20 09:44:01 -0400

    Implementing selected items in a table

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -40,6 +40,7 @@ const ConferenceEvent = () => {
         dispatch(decrementAvQuantity(index));
     };
 
+    // fonction pour gérer la sélection des repas
     const handleMealSelection = (index) => {
        const item = mealsItems[index];
         if (item.selected && item.type === "mealForPeople") {
@@ -52,14 +53,68 @@ const ConferenceEvent = () => {
         }
     };
 
+    // fonction pour récupérer les éléments sélectionnés par l'utilisateur
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+            if(item.quantity > 0){
+                items.push({...item, type: 'venue'})
+            }
+        });
+        avItems.forEach((item) => {
+            if (
+              item.quantity > 0 &&
+              !items.some((i) => i.name === item.name && i.type === "av")
+            ) {
+              items.push({ ...item, type: "av" });
+            }
+        });
+        mealsItems.forEach((item) => {
+            if (item.selected) {
+              const itemForDisplay = { ...item, type: "meals" };
+              if (item.numberOfPeople) {
+                itemForDisplay.numberOfPeople = numberOfPeople;
+              }
+              items.push(itemForDisplay);
+            }
+        });
+        return items;
     };
 
+    // récupération des éléments sélectionnés par le user
     const items = getItemsFromTotalCost();
 
     const ItemsDisplay = ({ items }) => {
-
+        <div className="display_box1">
+            { items.length === 0 && <p>No items selected</p> }
+            <table className="table_item_data">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Unit Cost</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>${item.cost}</td>
+                            <td>
+                                {item.type === "meals" || item.numberOfPeople
+                                ? ` For ${numberOfPeople} people`
+                                : item.quantity}
+                            </td>
+                            <td>{item.type === "meals" || item.numberOfPeople
+                                ? `${item.cost * numberOfPeople}`
+                                : `${item.cost * item.quantity}`}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     };
     const calculateTotalCost = (section) => {
         let totalCost = 0;
@@ -83,6 +138,12 @@ const ConferenceEvent = () => {
     const venueTotalCost = calculateTotalCost('venue');
     const avTotalCost = calculateTotalCost('av');
     const mealsTotalCost = calculateTotalCost('meals');
+
+    const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost
+    }
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
